6a02829cbe0aedee3553cafb50f5055304e5bcec
Nataliya Hrynchyshyn <160016021+NataliyaHr@users.noreply.github.com>
2024-08-20 13:47:24 +0200

    commit

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,39 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const delayInput = form.elements['delay'];
+    const stateInput = form.elements['state'];
+    const delay = Number(delayInput.value);
+    const state = stateInput.value;
+
+    createPromise(delay, state)
+        .then((delay) => {
+            iziToast.success({
+                title: 'Success',
+                message: `✅ Fulfilled promise in ${delay}ms`,
+            });
+        })
+        .catch((delay) => {
+            iziToast.error({
+                title: 'Error',
+                message: `❌ Rejected promise in ${delay}ms`,
+            });
+        });
+});
+
+function createPromise(delay, state) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === 'fulfilled') {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    });
+}
